de5267e8d347f614ff6a6a24e0f51d82e81a8465
AlekseyLu <luzyanin-ae@yandex.ru>
2024-05-16 14:39:56 +0300

    fix deploy

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/todo-list-react">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Home />
     </BrowserRouter>
   </React.StrictMode>
