15e42a129cf5cfb862dfc6b00755f7f63860df2f
AlekseyLu <luzyanin-ae@yandex.ru>
2024-05-16 14:57:10 +0300

    fix deploy

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { Home } from "../pages/Home/Home.tsx";
 
@@ -8,8 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/todo-list-react">
       <Home />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
