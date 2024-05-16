def81cd2542ec5a0914cbbb1f508d05d61024d8d
AlekseyLu <luzyanin-ae@yandex.ru>
2024-05-16 15:49:21 +0300

    fix deploy

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -6,7 +6,7 @@ export const NotFound = () => (
   <div className="flex items-center justify-center gap-4 flex-col w-full h-80 text-center text-slate-400">
     <h2 className="text-7xl">4 0 4</h2>
     <h2 className="text-3xl mb-[40px]">Страница не найдена</h2>
-    <Link to="/">
+    <Link to="/todo-list-react/">
       <ButtonCustom label="На главную" disabled={false} />
     </Link>
   </div>
